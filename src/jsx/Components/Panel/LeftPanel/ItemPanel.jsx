53325bcc55c4ed9786a71dd9861158eb619be732
zheng <zheng@zhengs-MacBook-Pro.local>
2021-06-10 15:12:56 -0400

    add formik for form

--- src/jsx/Components/Panel/LeftPanel/ItemPanel.jsx
+++ src/jsx/Components/Panel/LeftPanel/ItemPanel.jsx
@@ -1,18 +1,51 @@
 import React from "react";
 import { SelectControl } from "./SelectControl.jsx";
+import { useFormik } from 'formik';
 
 export const ItemPanel = (props) => {
+    const formik = useFormik(
+        {
+            initialValues: {
+                item_name: "",
+                column_name: ""
+            },
+            onSubmit: values => {
+                console.log(values);
+            },
+            validate: values => {
+                const errors = {};
+                if (!values.item_name) {
+                    errors.item_name = "Please enter an item name.";
+                }
+
+                if (!values.column_name) {
+                    errors.column_name = "Please select a column.";
+                }
+                return errors;
+            }
+        }
+    );
     return <>
         <div className="col-12 col-md-4 col-lg-4 col-xl-4 mt-4 eg-left-itempanel">
-            <div className="eg-additem-control">
-                <input type="text" className="eg-input" placeholder="ENTER ITEM" />            
-                <SelectControl />
-            </div>
-            <div className="eg-search-control">
-                <button className="eg-btn-additem">ADD ITEM</button>
-                <br/>
-                <label className="eg-lbl-search mt-5">SEARCH AN ITEM</label>
-                <input className="eg-search" placeholder="SEARCH ITEM"/>
+            <form onSubmit={formik.handleSubmit}>
+                <div className="eg-additem-control">
+                    <input type="text"
+                        className="eg-input"
+                        placeholder="ENTER ITEM"
+                        name="item_name"
+                        value={formik.values.item_name}
+                        onChange={formik.handleChange}
+                        autoComplete="none"
+                    />
+                    {formik.errors.item_name && formik.touched.item_name && <div className="eg-error">{formik.errors.item_name}</div>}
+                    <SelectControl formik={formik} />
+
+                    <button type="submit" className="eg-btn-additem">ADD ITEM</button>
+                </div>
+            </form>
+            <div className="eg-search-control">                
+                <label className="eg-lbl-search">SEARCH AN ITEM</label>
+                <input className="eg-search" placeholder="SEARCH ITEM" />
             </div>
         </div>
     </>
--- src/jsx/Components/Panel/LeftPanel/SelectControl.jsx
+++ src/jsx/Components/Panel/LeftPanel/SelectControl.jsx
@@ -1,28 +1,36 @@
 import React, { useState } from "react";
 
 export const SelectControl = (props) => {
+    const { formik } = props;
     const [open, setOpen] = useState(false);
+    const [columnName, setColumnName] = useState(!formik.values.column_name ? "CHOOSE COLUMN" : formik.values.column_name);
 
-
-
-    return <div className="eg-select mt-4">
+    return <div className="eg-select mt-4">        
         <div className="row align-items-center eg-select-default-item"
-            onClick={e => { setOpen(!open) }}>
+            onClick={e => { 
+                setOpen(!open);
+                formik.setFieldError("column_name", "");
+            }}>
             <div className="col-8">
-                <label>CHOOSE COLUMN</label>
+                <label>{columnName}</label>
             </div>
             <div className="col-4 eg-select-arrow">
-                {open ? <img src="assets/images/arrow-up.svg" /> : <img src="assets/images/arrow-down.svg" />}
-                {}
+                {open ? <img src="assets/images/arrow-up.svg" /> : <img src="assets/images/arrow-down.svg" />}                
             </div>
         </div>
+        {formik.errors.column_name && <div className="eg-error">{formik.errors.column_name}</div>}
 
         {open && ["COLUMN 1", "COLUMN 2"].map((item, idx) => {
-            return <div className={`row align-items-center eg-select-item ${idx===0 ? "" : "mt-1"}`}>
-            <div className="col-8">
-                <label>{item}</label>
+            return <div className={`row align-items-center eg-select-item ${idx === 0 ? "" : "mt-1"}`} onClick = {e => {
+                    const col_name = `COLUMN ${idx+1}`;
+                    setColumnName(col_name);
+                    setOpen(!open);
+                    formik.setFieldValue("column_name", col_name);
+                }}>
+                <div className="col-8">
+                    <label>{item}</label>
+                </div>
             </div>
-        </div>
         })}
     </div>
 }
